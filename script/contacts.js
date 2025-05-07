f379f68b5b0f7dc939afaef5ee229c4b6a24c83e
Tim Thiessen <timthiessen.2003@gmail.com>
2025-05-07 16:30:58 +0200

    addet sortfunction addet getFirstLetter function

--- script/contacts.js
+++ script/contacts.js
@@ -1,42 +1,17 @@
 let Contacts = [];
 
-function getContacts(data){
+function getContacts(data,){
     
     fetch('https://joinstorage-ef266-default-rtdb.europe-west1.firebasedatabase.app/contacts.json')
         .then(response => response.json())
         .then(data => {
-         
+            Contacts = data;
             renderContacts(data);
+        
         });
       
-       
+    
     return data;
-}
-
-function renderContacts(contacts){ 
-
-        let contactCards = '';
-        for (let index = 0; index < contacts.length; index++) {
-            const contact = contacts[index];
-            const initials = contact.name
-                .trim()
-                .split(' ')
-                .filter(word => word.length > 0)
-                .map(word => word[0].toUpperCase()); // Initialen aus dem Namen
-            contactCards += getContactCardTamplate(contact.name, contact.mail, initials);
-        }
-        document.getElementById("contact_card_section").innerHTML = contactCards;
-
-
-}
-
-
-function getContact(id){ //get contact by id
-
-}
-
-function addContact(id){
-
 }
 
 function updateDatabase(data){ //update contact by id
@@ -56,6 +31,122 @@ function updateDatabase(data){ //update contact by id
     });
 }
 
+/*function renderContacts(contacts){ 
+        let contactCards = '';
+        sortContacts(contacts);
+        let oldLetter = '';
+        for (let index = 0; index < contacts.length; index++) {
+            const contact = contacts[index];
+            const initials = contact.name
+                .trim()
+                .split(' ')
+                .filter(word => word.length > 0)
+                .map(word => word[0].toUpperCase()); 
+            let change = false;
+            let firstLetter = getFirstLetter(contact.name, oldLetter, change);
+
+            if(change) {
+                //contactCards += `<div class="contact_letter">${firstLetter}</div>`;
+                document.getElementById("contacts_section_letter").innerHTML = firstLetter; // Hole dir den ersten Buchstaben des Namens und stelle ihn in der Sektion dar
+                // Hole dir Id von Separator und stelle den ersten Buchstaben des Namens
+
+            }
+
+            contactCards += getContactCardTamplate(contact.name, contact.mail, initials, index);
+        }
+        
+        document.getElementById("contact_card_section").innerHTML = contactCards;
+
+}*/
+
+/*function renderContacts(contacts) { 
+    let contactCards = '';
+    sortContacts(contacts); // Kontakte sortieren
+    let oldLetter = ''; // Speichert den vorherigen Buchstaben
+
+    for (let index = 0; index < contacts.length; index++) {
+        const contact = contacts[index];
+        const initials = contact.name
+            .trim()
+            .split(' ')
+            .filter(word => word.length > 0)
+            .map(word => word[0].toUpperCase()); 
+
+        const firstLetter = contact.name.charAt(0).toUpperCase(); // Erster Buchstabe des Namens
+
+        // Wenn der Buchstabe wechselt, füge eine neue Überschrift hinzu
+        if (oldLetter !== firstLetter) {
+            contactCards += `<div class="contacts_section_header">${firstLetter}</div>`;
+            oldLetter = firstLetter; // Aktualisiere den alten Buchstaben
+        }
+
+        // Füge die Kontaktkarte hinzu
+        contactCards += getContactCardTamplate(contact.name, contact.mail, initials, index);
+    }
+
+    // Setze den HTML-Inhalt der Kontaktliste
+    document.getElementById("contact_card_section").innerHTML = contactCards;
+}*/
+
+function renderContacts(contacts) { 
+    let contactCards = '';
+    sortContacts(contacts); // Kontakte sortieren
+    let oldLetter = ''; // Speichert den vorherigen Buchstaben
+
+    for (let index = 0; index < contacts.length; index++) {
+        const contact = contacts[index];
+        const initials = contact.name
+            .trim()
+            .split(' ')
+            .filter(word => word.length > 0)
+            .map(word => word[0].toUpperCase()); 
+
+        const firstLetter = contact.name.charAt(0).toUpperCase(); // Erster Buchstabe des Namens
+
+        // Wenn der Buchstabe wechselt, füge eine neue Überschrift hinzu
+        if (oldLetter !== firstLetter) {
+            contactCards += `
+                <div class="contacts_section_header">
+                    <p class="contacts_section_letter">${firstLetter}</p>
+                </div>`;
+            oldLetter = firstLetter; // Aktualisiere den alten Buchstaben
+        }
+
+        // Füge die Kontaktkarte hinzu
+        contactCards += getContactCardTamplate(contact.name, contact.mail, initials, index);
+    }
+
+    // Setze den HTML-Inhalt der Kontaktliste
+    document.getElementById("contact_card_section").innerHTML = contactCards;
+}
+
+function sortContacts(contacts){ //sort contacts by name
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function getFirstLetter(name, oldLetter, change){ //get first letter of name
+
+    const firstLetter = name.charAt(0);
+
+    if (oldLetter !== firstLetter) {
+        change = true;
+    }
+    else {
+        change = false;
+    }
+    oldLetter = firstLetter;
+
+    return firstLetter;
+}
+
+function getContact(id){ //get contact by id
+    console.log(Contacts[id]);
+}
+
+function addContact(id){
+
+}
+
 function deleteContact(id){
 
 }
@@ -64,32 +155,6 @@ function editContact(id, newContactdata){ //add contact aufrufen + daten vom ang
 
 }
 
-function sortContacts(){ //sort contacts by name
-
-
+function renderViewCard(contacts){
+     
 }
-
-    /*console.log(contacts);
-    let contactCardName = document.getElementById("contact_name");
-    for (let index = 0; index < contacts.length; index++) {
-      let contactCardName = contacts[index].name;
-      contactCardName.innerHTML = contactCardName; 
-    }
-
-    console.log(contacts);
-    let contactCardMail = document.getElementById("contact_email");
-    for (let index = 0; index < contacts.length; index++) {
-      let contactsMail = contacts[index].mail;
-      contactCardMail.innerHTML = contactsMail; 
-    }
-
-    let contactsCard = contactCardMail+contactCardName;
-  
-    document.getElementById("contact_card_section").innerHTML = getContactCardTamplate() + contactsCard;*/
-    /*let contactCards = '';
-    for (let index = 0; index < contacts.length; index++) {
-        const contact = contacts[index];
-        const initials = contact.name.split(' ').map(word => word[0]); // Initialen aus dem Namen
-        contactCards += getContactCardTamplate(contact.name, contact.mail, initials);
-    }
-    document.getElementById("contact_card_section").innerHTML = contactCards;*/
