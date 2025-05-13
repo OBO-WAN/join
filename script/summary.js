75e875d17c5bf872a28090da61757ffadcac3472
OBO-WAN <kontakt@oboe-werkstatt.de>
2025-05-13 14:41:49 +0200

    responsive <nav>

--- script/summary.js
+++ script/summary.js
@@ -1,3 +1,16 @@
+async function initializeSummaryPage() {
+  try {
+    await initializeSummaryData();
+    setupSummaryUI();
+    setupHoverIcons();
+  } catch (error) {
+    console.error('Error initializing the summary page', error);
+  }
+}
+
+
+
+ 
 
   
    
