824ad15c95e5306d662c9815f9bbafe0d08e5af7
OBO-WAN <kontakt@oboe-werkstatt.de>
2025-05-14 18:00:29 +0200

    Enhanced <nav>

--- script/summary.js
+++ script/summary.js
@@ -9,6 +9,26 @@ async function initializeSummaryPage() {
 }
 
 
+/**
+ * Sets focus on a specified link and navigates to the given URL.
+ * At the same time, the active link in the sidebar is visually marked
+ * and the active page is stored in session storage.
+ *
+ * @param {string} url - The target URL to navigate to.
+ * @param {HTMLElement} element - The DOM element that contains the link.
+ */
+function toHrefFocus(url, element) {
+  document.querySelectorAll('.link-button').forEach((li) => {
+    li.classList.remove('active');
+  });
+
+  if (element) {
+    element.closest('li').classList.add('active');
+  }
+
+  sessionStorage.setItem('activePage', url);
+  window.location.href = url;
+}
 
  
 
